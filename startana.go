ec16da4820750c1f8569521b6d72eedf91820873
WeirdMagician <44853634+WeirdMagician@users.noreply.github.com>
2018-12-04 11:37:29 +0530

    Update startana.go

--- startana.go
+++ startana.go
@@ -1,33 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strings"
 )
 
-func main() {
-	start(os.Stdout)
+//Logentity is the struct to define components of a log
+type Logentity struct {
+	LogFile   string
+	TypeOfLog string
+	LogTime   string
+	RequestID string
+	Container string
+	Classname string
+	Value     string
 }
 
-func start(w io.Writer) {
-	//var filesdata string
-	var filesbyarr []byte
-	if len(os.Args) == 1 {
-		fmt.Fprintf(w, "No log files are provided")
-	} else {
-		for _, filename := range os.Args[1:] {
-			data, err := ioutil.ReadFile(filename)
-			if err != nil {
-				fmt.Fprintf(w, "\n\t Error Occurred while reaading the file %q\n the error is: %v", filename, err)
-			}
-			filesbyarr = append(filesbyarr, data...)
-			//filesdata +=
+//Data consists of whole output
+var Data []Logentity
+var delimeter = "\\[|\\]\\[|\\]:\\[|\\]:"
+var reg = regexp.MustCompile(delimeter)
 
-		}
-		//	strings.SplitAfter(string(data), "[console.accounts]")
-		fmt.Fprintf(w, "\n%s", string(filesbyarr))
+func main() {
+	filename := os.Args[1:2]
+	data, err := ioutil.ReadFile(filename[0])
+	if err != nil {
+		fmt.Printf("err value: %v", err)
 	}
+	filedata := string(data)
+	fileslice := strings.Split(filedata, "[console.accounts]")
+	for _, v := range fileslice {
+		//fmt.Println(v)
+		result := reg.Split(v, 7)
+		//fmt.Printf(strings.Join(result, "*************"))
+		if len(result) == 1 {
+			continue
+		}
+		oneenity := Logentity{"[console.accounts]", result[1], result[2], result[3], result[4], result[5], result[6]}
+		Data = append(Data, oneenity)
+	}
+	res2B, _ := json.Marshal(Data)
+	mode := int(0777)
 
+	ioutil.WriteFile("out.txt", res2B, os.FileMode(mode))
 }
